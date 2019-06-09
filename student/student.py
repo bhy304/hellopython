38b166c91f7ad0fe0bbac9239f8cc45e5f16eca1
bhy304 <bhy0512@gmail.com>
2019-06-10 01:50:26 +0900

    csv 파일읽고 쓰기, 학생정보 sorting, 학점 및 총점,평균, 평균 이상 출력 기능 구현

---
+++ student/student.py
@@ -0,0 +1,93 @@
+from functools import reduce
+
+students = []
+# print(g_grades)
+class Student:
+    grade = ''
+    def __init__(self, line):
+        name, gender, age, score = line.strip().split(',')
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.score = int(score)
+
+    def __str__(self):
+        return "{}\t{}\t{}\t{}\t{}".format(self.name, self.gender, self.age, self.score, self.grade)
+
+    def write_file():
+        with open("students.csv","w",encoding="utf8") as file:
+            file.write("이름,성별,나이,성적\n")
+            file.write("김일수,남,23,80\n")
+            file.write("홍길순,여,25,75\n")
+            file.write("박길동,남,45,55\n")
+            file.write("최순식,남,34,90\n")
+            file.write("강감찬,남,90,100\n")
+            file.write("갑돌이,남,38,65\n")
+            file.write("박삼순,여,31,70\n")
+            file.write("김갑순,여,28,85\n")
+            file.write("이갑순,여,28,85\n")
+            file.write("이순신,남,88,89")
+
+    def read_students():
+        with open("students.csv","r",encoding="utf8") as file:
+            for i, line in enumerate(file):
+                # print(line)
+                if i == 0: continue
+                students.append(Student(line))
+
+    def print_students():
+        print("1. 학생정보")
+        print("이름\t성별\t나이\t성적\t학점")
+        print("----\t----\t----\t----\t----")
+        for s in students:
+            print(s)
+
+    # 학생정보를 성적순으로 sorting하는 메소드 (sort)
+    def sorting_students():
+        print("2. Sorting")
+        students.sort(key=lambda stu: stu.score, reverse=True)
+        print("이름\t성별\t나이\t성적\t학점")
+        print("----\t----\t----\t----\t----")
+        for s in students:
+            print(s)
+
+    # 학생 성적으로 학점 매기는 메소드 (map, lambda)
+    def grading_students(self): 
+        g_grades = ['A', 'B', 'C', 'D', 'F']
+        g_grades.reverse()       
+        if self.score == 100:
+            self.grade = 'A+'
+        else:
+            self.grade = g_grades[self.score // 10 - 5]
+        # grade_dict = {10:'A+',9:'A',8:'B',7:'C',6:'D'}
+        # grade = grade_dict.get(self.score // 10, 'F')
+
+    # print(list(map(lambda stu: stu.grading_students(), students)))
+    
+    
+    # 전체 학생의 총점과 평균을 구하는 메소드. (reduce,lambda)
+    
+    def get_sum(): 
+        print("3.합계와 평균")
+        total = reduce(lambda x, y: (x if type(x) == int else x.score) + y.score, students)
+        avg = total / len(students)
+        print("합계=", total)
+        print("평균=", avg)
+
+    # 평균 이상인 학생정보를 출력하는 메소드 (filter, lambda)
+    def print_above_avg(self):
+        print("4.평균 이상")
+        std_avg = filter(lambda x : x.score >= self.avg, self.stu)
+        for s in std_avg:
+            print(s)
+        # for s in students:
+        #     if s.score >= avg:
+        #         print(s)
+
+# write_file()
+Student.read_students()
+Student.print_students()
+Student.sorting_students()
+Student.get_sum()
+Student.print_above_avg()
+
